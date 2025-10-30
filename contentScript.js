f253b49ec2f1b4c74f7af9e8f655e9c46ec76b75
winston <winstondegreef@gmail.com>
2025-10-30 22:05:40 +0100

    copy over boas's change to popup.js to the code thats now being run in contentscript.js

--- contentScript.js
+++ contentScript.js
@@ -249,6 +249,7 @@
         try {
           if (el.matches && el.matches('div.exercise-header-wrapper.d-flex.justify-content-between')) return true;
           if (el.matches && el.matches('p.fs-7.fst-italic')) return true;
+          if (el.matches && el.matches('grapp-question-answer-field')) return true;
         } catch (e) {}
         return false;
       }
